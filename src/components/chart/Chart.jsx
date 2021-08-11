1297017feee0f1c7cb23aa6f87bc0493b364081d
Temi-t <temiemmanuel@yahoo.com>
2021-08-11 08:19:45 +0100

    colored background

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -1,6 +1,5 @@
 import './chart.css';
-import { LineChart, BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-// import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import {  BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
 
 
 function Chart({title, title2, data,dataKey, grid}) {
@@ -14,30 +13,13 @@ function Chart({title, title2, data,dataKey, grid}) {
             </div>
             <ResponsiveContainer width="100%" aspect={4 / 1.1}>
                 <BarChart data={data}>
-                    {/* <XAxis dataKey="name" stroke="blue" /> */}
                     <XAxis dataKey="month"  />
-
                     <YAxis dataKey="Income" />
                     <Bar dataKey="Income" fill="#8884d8" barSize={20}/>
                     <Tooltip />
                     {/* <Legend /> */}
                 </BarChart>
             </ResponsiveContainer>
-            {/* <ResponsiveContainer>
-                <BarChart width={730} height={250} data={data}>
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="month" />
-                    <YAxis />
-                    <Tooltip />
-                    <Legend />
-                    <Bar dataKey="Income" fill="#8884d8" />
-                </BarChart>
-            </ResponsiveContainer> */}
-            {/* <ResponsiveContainer width="100%" height="100%">
-                <BarChart width={150} height={40} data={data}>
-                    <Bar dataKey="Income" fill="#8884d8" />
-                </BarChart>
-            </ResponsiveContainer> */}
             
         </div>
     )
